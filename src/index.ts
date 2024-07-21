86f3a6497a0f99308c3c488d4be4dfba39ed760b
sakitayu <sakitakitakita@gmail.com>
2024-07-21 21:48:53 +0900

    簡単なエンドポイントを作成

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello express')
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+const PORT = 8080;
+
+app.get('/allTodos', (req: Request,res:Response) => {
+  return res.send('Todos');
+});
+
+app.listen(PORT, () => console.log('server is running🚀'));
