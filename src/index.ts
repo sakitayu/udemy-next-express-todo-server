cebea6706297ab19e02f8085da0b01517cc1ed56
sakitayu <sakitakitakita@gmail.com>
2024-08-27 14:50:20 +0900

    Todoを作成するAPIエンドポイントを作成

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,27 @@ import { PrismaClient } from '@prisma/client';
 const app: Express = express();
 const PORT = 8080;
 
+app.use(express.json());
 const prisma = new PrismaClient();
 
-app.get('/allTodos', async (req: Request,res:Response) => {
+app.get('/allTodos', async (req: Request, res: Response) => {
   const allTodos = await prisma.todo.findMany();
   return res.json(allTodos);
+});
+
+app.post('/createTodo', async (req: Request, res: Response) => {
+  try {
+    const {title, isCompleted} = req.body;
+    const createTodos = await prisma.todo.create({
+      data: {
+        title,
+        isCompleted,
+      },
+    });
+    return res.json(createTodos);
+  } catch (e) {
+    return e;
+  }
 });
 
 app.listen(PORT, () => console.log('server is running🚀'));
