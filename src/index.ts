75829af209c5e244c78f8aa8c21c2fd04414cb03
sakitayu <sakitakitakita@gmail.com>
2024-08-27 15:41:03 +0900

    Todoを編集するAPIエンドポイントを作成

--- src/index.ts
+++ src/index.ts
@@ -16,17 +16,35 @@ app.get('/allTodos', async (req: Request, res: Response) => {
 app.post('/createTodo', async (req: Request, res: Response) => {
   try {
     const {title, isCompleted} = req.body;
-    const createTodos = await prisma.todo.create({
+    const createTodo = await prisma.todo.create({
       data: {
         title,
         isCompleted,
       },
     });
-    return res.json(createTodos);
+    return res.json(createTodo);
   } catch (e) {
-    return e;
+    return res.status(400).json(e);
+  }
+});
+
+app.put('/editTodo/:id', async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const {title, isCompleted} = req.body;
+    const editTodo = await prisma.todo.update({
+      where: {
+        id
+      },
+      data: {
+        title,
+        isCompleted,
+      },
+    });
+    return res.json(editTodo);
+  } catch (e) {
+    return res.status(400).json(e);
   }
 });
 
 app.listen(PORT, () => console.log('server is running🚀'));
-
